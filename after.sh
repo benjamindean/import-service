5caa6c27eed25f894ccd78499aa6e1163644567d
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-09-29 14:44:10 +0200

    Change path for worker

--- after.sh
+++ after.sh
@@ -12,11 +12,11 @@ sudo apt-get install supervisor -y > /dev/null
 echo "
 [program:lumen-worker]
 process_name=%(program_name)s_%(process_num)02d
-command=php /home/vagrant/imports/artisan queue:work --tries=3
+command=php /home/vagrant/importservice/artisan queue:work --tries=3
 autostart=true
 autorestart=true
 numprocs=4
-stdout_logfile=/home/vagrant/imports/storage/logs/worker.log
+stdout_logfile=/home/vagrant/importservice/storage/logs/worker.log
 " > /etc/supervisor/conf.d/lumen-worker.conf
 
 echo "Starting Supervisor"
